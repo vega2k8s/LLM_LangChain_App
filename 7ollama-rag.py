94a913641e490bc0e87b75b16512944bb26c5bf8
vega2k8s <vega2k8s@gmail.com>
2025-06-13 17:27:02 +0900

    ollama rag 2

--- 7ollama-rag.py
+++ 7ollama-rag.py
@@ -83,8 +83,8 @@ if uploaded_file is not None:
     retriever = vector.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
     # LLM 정의
-    llm = ChatOllama(model="deepseek-r1:1.5b")
-    #llm = ChatOllama(model="qwen2.5:1.5b")
+    #llm = ChatOllama(model="deepseek-r1:1.5b")
+    llm = ChatOllama(model="qwen3:1.7b")
 
     # # 시스템 프롬프트 정의
     # system_prompt = (
